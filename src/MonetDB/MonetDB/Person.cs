97f788aabc5397b2acbee01bc5883551ef8136b3
Cosimo Augustoni <cosimo.augustoni@gmail.com>
2022-11-28 17:54:49 +0100

    Demo App

---
+++ src/MonetDB/MonetDB/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace MonetDB;
+
+internal class Person
+{
+    public Person(int id, string name, int yearsOfExperience)
+    {
+        Id = id;
+        Name = name;
+        YearsOfExperience = yearsOfExperience;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int YearsOfExperience { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id: {this.Id,-3} | Name: {this.Name,-15} | Berufserfahrung: {this.YearsOfExperience}";
+    }
+}
---
+++ src/MonetDB/MonetDB/Program.cs
@@ -0,0 +1,71 @@
+﻿using MonetDb.Mapi;
+
+namespace MonetDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var conn = new MonetDbConnection("Host=localhost;port=50000;Database=demo;username=monetdb;password=monetdb");
+            conn.Open();
+
+            ReCreateTablePerson(conn);
+            CreatePersons(conn);
+
+            var persons = ReadAllPersons(conn);
+            Console.WriteLine("Personen");
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            foreach (var person in persons) 
+                Console.WriteLine(person);
+
+            var avgYearsOfExperience = GetAverageYearsOfExperience(conn);
+            Console.WriteLine($"Durschnittliche Berufserfahrung: {avgYearsOfExperience}");
+        }
+
+        private static float GetAverageYearsOfExperience(MonetDbConnection conn)
+        {
+            var cmd = new MonetDbCommand("SELECT AVG(YearsOfExperience) FROM Person;", conn);
+            return cmd.ExecuteScalar() is float ? (float) cmd.ExecuteScalar() : throw new Exception("Durschnittliche Berufserfahrung konnte nicht bestummen werden");
+        }
+
+        private static List<Person> ReadAllPersons(MonetDbConnection conn)
+        {
+            var cmd = new MonetDbCommand("SELECT * FROM Person;", conn);
+            using var reader = cmd.ExecuteReader();
+            var persons = new List<Person>();
+            while (reader.Read())
+            {
+                persons.Add(new Person(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2)));
+            }
+
+            return persons;
+        }
+
+        private static void CreatePersons(MonetDbConnection conn)
+        {
+            var cmd = new MonetDbCommand("INSERT INTO Person VALUES(1, 'Hans', 4);", conn);
+            cmd.ExecuteNonQuery();
+            cmd = new MonetDbCommand("INSERT INTO Person VALUES(2, 'Petra', 2);", conn);
+            cmd.ExecuteNonQuery();
+            cmd = new MonetDbCommand("INSERT INTO Person VALUES(3, 'Ueli', 0);", conn);
+            cmd.ExecuteNonQuery();
+            cmd = new MonetDbCommand("INSERT INTO Person VALUES(4, 'Melanie', 6);", conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        private static void ReCreateTablePerson(MonetDbConnection conn)
+        {
+            var cmd = new MonetDbCommand("DROP TABLE Person;", conn);
+            cmd.ExecuteNonQuery();
+
+            cmd = new MonetDbCommand(@"CREATE TABLE Person(
+Id INT NOT NULL PRIMARY KEY,
+Name VARCHAR(200),
+YearsOfExperience INT
+);", conn);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
